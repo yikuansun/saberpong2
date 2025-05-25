24b094877d33d3564f50a3d0ce48088eecd7d4c9
Yikuan Sun <yikuansun2015@gmail.com>
2025-05-24 23:38:48 -0400

    Create cool laser
    Biggest improvement from v1

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -28,17 +28,21 @@
     let ball = {
         x: 1920 / 2,
         y: 1080 / 2,
-        radius: 10,
+        radius: 5,
         angle: 0,
-        color: "green",
-    }
+        color: "hsl(100deg, 100%, 50%)",
+    };
+    let ballPastPositions = [{x: ball.x, y: ball.y}];
+    let laserLength = 150;
 
     /** @type {Object.<string, boolean>} */
     let keysPressed = {};
 
     let deltaTime = 0; // velocities are in pixels per second
     let lastTime = (new Date()).getTime();
+    let frameCount = 0;
     const saberSpeed = 444;
+    const ballSpeed = 400;
     function mainGameLoop() {
         let startTime = (new Date()).getTime();
         deltaTime = (startTime - lastTime) / 1000;
@@ -60,6 +64,15 @@
             p2.y += saberSpeed * deltaTime;
             if (p2.y > 1080 - p2.height) p2.y = 1080 - p2.height;
         }
+
+        ball.x += ballSpeed * deltaTime * Math.cos(ball.angle);
+        ball.y += ballSpeed * deltaTime * Math.sin(ball.angle);
+        ballPastPositions.push({x: ball.x, y: ball.y});
+        if (ballPastPositions.length > laserLength / (ballSpeed * deltaTime)) {
+            ballPastPositions.shift();
+        }
+
+        frameCount++;
         requestAnimationFrame(mainGameLoop);
     }
 
@@ -103,9 +116,16 @@
         style:border-radius="100px" style:filter="blur(1px)"></div>
 
     <!-- laser -->
-    <div style:width="{ball.radius * 2}px" style:height="{ball.radius * 2}px" style:background-color={ball.color}
-        style:position="absolute" style:top="{ball.y - ball.radius}px" style:left="{ball.x - ball.radius}px"
-        style:border-radius="100px" style:filter="blur(1px)"></div>
+    <div style:mix-blend-mode="screen" style:filter="drop-shadow(0 0 5px {ball.color}) drop-shadow(0 0 15px {ball.color})">
+        <div style:width="{ball.radius * 2}px" style:height="{ball.radius * 2}px" style:background-color={ball.color}
+            style:position="absolute" style:top="{ball.y - ball.radius}px" style:left="{ball.x - ball.radius}px"
+            style:border-radius="100px" style:filter="blur(1px)"></div>
+        {#each ballPastPositions as pastPosition}
+            <div style:width="{ball.radius * 2}px" style:height="{ball.radius * 2}px" style:background-color={ball.color}
+                style:position="absolute" style:top="{pastPosition.y - ball.radius}px" style:left="{pastPosition.x - ball.radius}px"
+                style:border-radius="100px" style:filter="blur(1px)"></div>
+        {/each}
+    </div>
 
     <div style:position="fixed" style:top="10px" style:left="10px" style:color="white">
         Delta Time: {deltaTime.toFixed(3)}
