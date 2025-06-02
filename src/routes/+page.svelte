3ecd69e215ee1722c9ff3f9035ebdac721451bf6
Yikuan Sun <yikuansun2015@gmail.com>
2025-06-01 20:09:26 -0400

    Test autoplay policy
    Cool

---
+++ src/routes/+page.svelte
@@ -0,0 +1,34 @@
+<script>
+    import blankSound from "$lib/assets/sounds/nothing.mp3";
+    import { onMount } from "svelte";
+    import { goto } from "$app/navigation";
+
+    let autoplayAllowed = true;
+
+    onMount(() => {
+        let aud = new Audio(blankSound);
+        aud.addEventListener("canplaythrough", () => {
+            aud.play().then(() => {
+                // media autoplay allowed
+                goto("./menu");
+            }).catch((e) => {
+                // media autoplay not allowed, need user interaction
+                console.log(e.message);
+                autoplayAllowed = false;
+            });
+        });
+    });
+</script>
+
+{#if !autoplayAllowed}
+    <button style:position="fixed"
+        style:left="50vw" style:top="50vh"
+        style:transform="translate(-50%, -50%)"
+        on:click={() => { goto("./menu"); }}>
+        Run Game
+    </button>
+{:else}
+    <div style:width="100vw" style:height="100vh"
+        style:position="fixed" style:top="0" style:left="0"
+        style:background-color="black"></div>
+{/if}
